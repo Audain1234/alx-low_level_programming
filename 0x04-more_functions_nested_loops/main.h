77a7fca977e20bbaf1c6b78ad868b3f82f9575e0
/Audain1234 </audaintadiwanashe@gmail.com>
2023-03-23 18:01:00 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
 int _putchar(char c);
 
 int _isupper(int c);
@@ -26,6 +21,3 @@ void print_square(int size);
 void print_triangle(int size);
 
 void print_number(int n);
-
-
-#endif
